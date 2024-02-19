6f5ac69718ffbab804904f659f124b19022b436b
yaswanth <yaswanthm@presidio.com>
2024-02-19 11:52:21 +0530

    update on handler and yaml

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,15 +1,85 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import AWS from "aws-sdk";
 
-export const hello = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
+const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName = "ProductsTable";
+const headers = {
+  "content-type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "*",
+  "Access-Control-Allow-Headers": "*",
+};
+
+export const createProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log("create product triggered");
+  try {
+    if (event.httpMethod === "OPTIONS") {
+      console.log("Inside options");
+      return {
+        statusCode: 200,
+        headers: headers,
+        body: "",
+      };
+    }
+
+    const reqBody = JSON.parse(event.body as string);
+    console.log("request body is " + reqBody);
+    const product = {
+      ...reqBody,
+    };
+
+    await docClient
+      .put({
+        TableName: tableName,
+        Item: product,
+      })
+      .promise();
+
+    return {
+      statusCode: 201,
+      headers,
+      body: JSON.stringify(product),
+    };
+  } catch (e) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(e),
+    };
+  }
+};
+
+const fetchProductById = async (id: string) => {
+  console.log("fetchProduct by Id triggered");
+  const output = await docClient
+    .get({
+      TableName: tableName,
+      Key: {
+        productID: id,
       },
-      null,
-      2,
-    ),
-  };
+    })
+    .promise();
+
+  if (!output.Item) {
+    return { statusCode: 400, error: "not found" };
+  }
+
+  return output.Item;
+};
+
+export const getProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log("get product triggered");
+  try {
+    const product = await fetchProductById(event.pathParameters?.id as string);
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(product),
+    };
+  } catch (e) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(e),
+    };
+  }
 };
